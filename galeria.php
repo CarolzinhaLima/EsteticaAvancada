03707c8ab3767d810031e873b62011a7c55b0ab2
DIEGO JUNIOR EVANGELISTA CAXIADO <diego.jecaxiado@senacsp.edu.br>
2023-08-15 14:11:42 -0300

    galeria

--- galeria.php
+++ galeria.php
@@ -8,141 +8,44 @@
 <?php
   include('includes/menu.php');
   ?>
-  <main>
-   <style>
-   #galeria{
-    margin: 7% 0 4%;
-    font-family: 'Inter';
-    font-style: normal;
-    text-align: center;
-    color: #F77692;
-    font-size: 50px;
-    }
-    .row {
-      display: flex;
-      margin-left: 8em;
-      width: 70%;
-      flex-wrap: nowrap;
-      padding: 0%; 
-    }
+  
+  <body>
+    <div class="flex-container">
+    <div class="slide hi-slide">
+        <div class="hi-prev"></div>
+        <div class="hi-next"></div>
+        <ul>
+            <li><img src="img/img1.jpg" alt=""></li>
+            <li><img src="img/img2.webp" alt=""></li>
+            <li><img src="img/img3.webp" alt=""></li>
+            <li><img src="img/img4.jpg" alt=""></li>
+            <li><img src="img/img5.jpg" alt=""></li>
+            <li><img src="img/img6.jpg" alt=""></li>
+            <li><img src="img/img7.webp" alt=""></li>
+            <li><img src="img/img8.jpg" alt=""></li>
+            <li><img src="img/img9.jpg" alt=""></li>
+            <li><img src="img/img10.jpg" alt=""></li>
+            <li><img src="img/img3.webp" alt=""></li>
+            <li><img src="img/img8.jpg" alt=""></li>
+            <li><img src="img/img2.webp" alt=""></li>
+            <li><img src="img/img1.jpg" alt=""></li>
+        </ul>
+    </div>
+</div>
+  
+    <script src="https://code.jquery.com/jquery-3.7.0.js">
+    </script>
    
-   </style>
- <h1 id="galeria">Galeria</h1>
- <div class="row">
-    <!--carousel 1--> 
-    <div class="col-md-5 col-lg-5 d-none d-sm-block">
-       <div class="container-slider">
-          <div class="divslider" style="background-color: black;">
-             <div id="carouselExampleIndicators" data-interval="5000" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                   <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                   <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                   <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>  
-                <div class="carousel-inner">
-                   <div class="carousel-item active">
-                      <img class="d-block w-100" src="https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg" alt="Primeiro Slide">
-                      <div class="carousel-caption d-none d-md-block">
-                         <h5>Pães Frescos</h5>
-                         <p>pão</p>
-                      </div>
-                   </div>
-                   <div class="carousel-item">
-                      <img class="d-block w-100" src="https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg" alt="Segundo Slide">
-                   </div>
-                   <div class="carousel-item">
-                      <img class="d-block w-100" src="https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg" alt="Terceiro Slide">
-                   </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                   <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                   <span class="sr-only">Próximo</span>
-                </a>
-             </div>
-          </div>
-       </div>
-    </div>
- 
-    <!--carousel 2--> 
-    <div class="col-md-5 col-lg-5 d-none d-sm-block">
-       <div class="container-slider">
-          <div class="divslider" style="background-color: black;">
-             <div id="carouselExampleIndicators2" data-interval="5000" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                   <li data-target="#carouselExampleIndicators2" data-slide-to="0" class="active"></li>
-                   <li data-target="#carouselExampleIndicators2" data-slide-to="1"></li>
-                   <li data-target="#carouselExampleIndicators2" data-slide-to="2"></li>
-                </ol>  
-                <div class="carousel-inner">
-                   <div class="carousel-item active">
-                      <img class="d-block w-100" src="https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg" alt="Primeiro Slide">
-                      <div class="carousel-caption d-none d-md-block">
-                         <h5>Pães Frescos</h5>
-                         <p>pão</p>
-                      </div>
-                   </div>
-                   <div class="carousel-item">
-                      <img class="d-block w-100" src="https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg" alt="Segundo Slide">
-                   </div>
-                   <div class="carousel-item">
-                      <img class="d-block w-100" src="https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg" alt="Terceiro Slide">
-                   </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators2" role="button" data-slide="prev">
-                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                   <span class="sr-only">Anterior</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators2" role="button" data-slide="next">
-                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                   <span class="sr-only">Próximo</span>
-                </a>
-             </div>
-          </div>
-       </div>
-       <h2></h2>
-    </div>
-           <!--carousel 3--> 
-    <div class="col-md-5 col-lg-5 d-none d-sm-block">
-        <div class="container-slider">
-           <div class="divslider" style="background-color: black;">
-              <div id="carouselExampleIndicators3" data-interval="5000" class="carousel slide" data-ride="carousel">
-                 <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                 </ol>  
-                 <div class="carousel-inner">
-                    <div class="carousel-item active">
-                       <img class="d-block w-100" src="https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg" alt="Primeiro Slide">
-                       <div class="carousel-caption d-none d-md-block">
-                          <h5>Pães Frescos</h5>
-                          <p>pão</p>
-                       </div>
-                    </div>
-                    <div class="carousel-item">
-                       <img class="d-block w-100" src="https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg" alt="Segundo Slide">
-                    </div>
-                    <div class="carousel-item">
-                       <img class="d-block w-100" src="https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg" alt="Terceiro Slide">
-                    </div>
-                 </div>
-                 <a class="carousel-control-prev" href="#carouselExampleIndicators3" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Anterior</span>
-                 </a>
-                 <a class="carousel-control-next" href="#carouselExampleIndicators3" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Próximo</span>
-                 </a>
-              </div>
-           </div>
-        </div>
-     </div>
-    </div>
+
+    <script src="jquery.hislide.js">
+    </script>
+
+    <script>
+        $('.slide').hiSlide();
+    </script>
+
+</body>
+   
     <footer>
     <?php
    include('includes/rodape.php');
--- includes/head.php
+++ includes/head.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="mobile/menuMobile.css">
     <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="css/rodape.css">
+    <link rel="stylesheet" href="css/carrosel.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
@@ -21,4 +22,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/servicoSelecionado.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 </head>
